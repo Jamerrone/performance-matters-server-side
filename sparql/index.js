99bbe53254aceaa3d9fa8d63489846515ba02e55
James Peter Perrone Jefferies <james.p.perrone@gmail.com>
2018-03-22 19:20:24 +0100

    Minify HTML

--- sparql/index.js
+++ sparql/index.js
@@ -6,9 +6,18 @@ module.exports.firstRequest = async (req, res) => {
   req.query.sn = req.query.sn || 'aagtdorperpad'
   req.query.id = req.query.id || '5'
   data.details = await sparql.fetchStreetDetails(req.query.sn, req.query.id)
-  res.render('index', {
-    streets: data.streets,
-    details: data.details,
-    streetName: Object.values(data.details)[0][0].streetName.value
-  })
+  res.render(
+    'index',
+    {
+      streets: data.streets,
+      details: data.details,
+      streetName: Object.values(data.details)[0][0].streetName.value
+    },
+    (err, html) => {
+      if (err) {
+        console.log(err)
+      }
+      res.send(html)
+    }
+  )
 }
